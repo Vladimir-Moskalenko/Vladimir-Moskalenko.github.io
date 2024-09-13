dd8f176b11394537e9d8fd793ee427b56039d81a
Vladimir Moskalenko <93604107+Vladimir-Moskalenko@users.noreply.github.com>
2024-09-13 09:19:23 -0700

    Release 1.0 of the installer!
    Full /System/Application app icon support + Finder added!

--- blocks/Files/8/install.sh
+++ blocks/Files/8/install.sh
@@ -1,4 +1,5 @@
 # Made by \/\\\
+dir=$PWD
 
 installAquaTheme()
 {
@@ -37,35 +38,14 @@ sudo bless --mount "$HOME/lvmnt/System/Library/CoreServices/" --setBoot --create
 echo Installation successful! Reboot to apply changes.
 }
 
-
 installAquaIcons()
 {
 cd ~/Downloads
-read -p "Find the target drive name in the list above (the one WITHOUT the - Data suffix), then enter the #s# values. Then, type them below: " diskNums
-sudo mount -o nobrowse -t apfs /dev/disk$diskNums ~/lvmnt
-
-echo "Copying App icons..."
-sudo cp App\ Store.icns ~/lvmnt/System/Applications/App\ Store.app/Contents/Resources/AppIcon.icns
-sudo cp Automator.icns ~/lvmnt/System/Applications/Automator.app/Contents/Resources/AppIcon.icns
-sudo cp Calculator.icns ~/lvmnt/System/Applications/Calculator.app/Contents/Resources/AppIcon.icns
-sudo cp Chess.icns ~/lvmnt/System/Applications/Chess.app/Contents/Resources/AppIcon.icns
-sudo cp Contacts.icns ~/lvmnt/System/Applications/Contacts.app/Contents/Resources/AppIcon.icns
-sudo cp Dictionary.icns ~/lvmnt/System/Applications/Dictionary.app/Contents/Resources/AppIcon.icns
-sudo cp FaceTime.icns ~/lvmnt/System/Applications/FaceTime.app/Contents/Resources/AppIcon.icns
-sudo cp FindMy.icns ~/lvmnt/System/Applications/Find\ My.app/Contents/Resources/AppIcon.icns
-sudo cp Font\ Book.icns ~/lvmnt/System/Applications/Font\ Book.app/Contents/Resources/AppIcon.icns
-sudo cp Home.icns ~/lvmnt/System/Applications/Home.app/Contents/Resources/AppIcon.icns
-sudo cp Image\ Capture.icns ~/lvmnt/System/Applications/Image\ Capture.app/Contents/Resources/AppIcon.icns
-sudo cp Launchpad.icns ~/lvmnt/System/Applications/Launchpad.app/Contents/Resources/AppIcon.icns
-sudo cp Mail.icns ~/lvmnt/System/Applications/Mail.app/Contents/Resources/AppIcon.icns
-sudo cp Maps.icns ~/lvmnt/System/Applications/Maps.app/Contents/Resources/AppIcon.icns
-sudo cp Messages.icns ~/lvmnt/System/Applications/Messages.app/Contents/Resources/AppIcon.icns
-sudo cp Mission\ Control.icns ~/lvmnt/System/Applications/Mission\ Control.app/Contents/Resources/AppIcon.icns
-sudo cp Music.icns ~/lvmnt/System/Applications/Music.app/Contents/Resources/AppIcon.icns
-sudo cp News.icns ~/lvmnt/System/Applications/News.app/Contents/Resources/AppIcon.icns
-#sudo cp .icns ~/lvmnt/System/Library/Applications/.app/AppIcon.icns
-sudo bless --mount ~/lvmnt --bootefi --create-snapshot
-sudo bless --mount "$HOME/lvmnt/System/Library/CoreServices/" --setBoot --create-snapshot > /dev/null 2>&1
+echo Downloading installIcon tool...
+git clone https://github.com/Vladimir-Moskalenko/LicK
+cd LicK
+chmod +x installIcons.sh
+./installIcons.sh
 }
 
 
